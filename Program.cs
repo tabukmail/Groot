f8367eeb704d86dd861ae1c6f3fe36eee34e44db
bali <tabuk@mail.ee>
2025-01-07 11:36:36 +0100

    1st commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {   
+        
+       Tree tree = new Tree();
+       tree.addNode(1,"first node");
+       tree.addNode(2,"second node");
+
+       
+       
+       foreach (Node element in tree.getList())
+       {
+           Console.WriteLine(element.getParentID());
+       }
+
+
+       
+      
+
+
+    }
+}

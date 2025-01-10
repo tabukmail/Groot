117b8a681a5fade5b48dc74b6ee1f1140ee0fcf6
bali <tabuk@mail.ee>
2025-01-10 17:59:34 +0100

    nested version

--- NStree/Node2.cs
+++ NStree/Node2.cs
@@ -1,59 +1,124 @@
+using System.Collections;
+
 namespace ConsoleApp1.NStree;
 
-public class Node2
+public class Node2 : IComparable
 {
-    private int _number = 0;
-
-    public int getNumber()
-    {
-        return _number;
-    }
-
-    private void setNumberzzz(int number)
-    {
-        _number = number;
-    }
-
-    // ---------------------------------------------------------------------
-    public class Tree2
-    {
-        private int numm = 1;
-
-        private void setNum(int num)
-        {
-            numm = num;
-        }
-        
-        void someMethod()
-        {
-            Node2 node = new Node2();
-            node.getNumber();
-            node.setNumberzzz(1);
+    private int _parentId;
+    private int _id;
+    private int _leftKey;
+    private int _rightKey;
+    private int _level;
+    private string _name;
+    private NodeValues _nodeValues= new NodeValues();
     
-            Node2.Tree2 tree = new Node2.Tree2();
-            tree.setNum(1);
+
+    public int GetParentId() => _parentId;
+    public int GetId() => _id;
+    public int GetLevel() => _level;
+    
+    public string GetName() => _name;
+    public void SetName(string name) => _name = name;
+    
+    public int GetLeft_Key() => _leftKey;
+    private void setLeft_Key(int key) => _leftKey = key;
+
+    public int GetRight_Key() => _rightKey;
+    private void setRight_Key(int key) => _rightKey = key;
+
+    
+    
+    public NodeValues GetNodeValues() => _nodeValues;
+    
+  
+    public Node2(int parentId, int id, int leftKey, int rightKey, int level, string name)
+    {
+        _parentId = parentId;
+        _id = id;
+        _leftKey = leftKey;
+        _rightKey = rightKey;
+        _level = level;
+        _name = name;
+    }
+
+    
+    public Node2(int parentId, string name)
+    {
+        _parentId = parentId;
+        _name = name;
+    }
+
+   
+
+
+    public int CompareTo(object? incomingobject) 
+    { 
+        Node2 incomingNode = incomingobject as Node2 ?? throw new InvalidOperationException(); 
+        return this._leftKey.CompareTo(incomingNode._leftKey); 
+    } 
+        /// ===================================== bested class ============================
+        public class Tree2 
+            {
+                private ArrayList _tree = new ArrayList{new Node2(0, 1, 1, 2, 0, "root")};  
+                
+                public ArrayList GetTree()
+                {
+                    return _tree;
+                }
             
-        
-        }
-        
-        
-    }
-    // ---------------------------------------------------------------------
+               
+                public void AddNode(int parentId, string newNodeName)
+                {
+                    
+                    if ((parentId > _tree.Count) || (parentId  <= 0)){
+                        Console.WriteLine($":: -> {parentId } is out of possible range");
+                        return;
+                    }
+                    
+                    var lefthKey = 0;
+                    var rightKey = 0;
+                    var level = 0;
+                    var parentRightKey = 0;
+                    var parentLefthKey = 0;
+                    
+                   foreach (Node2? node in _tree.ToArray())
+                    {
+                        if (node != null && parentId == node.GetId())
+                        {
+                            parentRightKey = node.GetRight_Key();
+                            parentLefthKey = node.GetLeft_Key();
+                            lefthKey = node.GetRight_Key();
+                            rightKey = lefthKey + 1;
+                            level = node.GetLevel() + 1;
+                            node.setRight_Key(node.GetRight_Key() + 2);
+                            
+                        }
+                    }
+                   
+                    foreach (Node2? node in _tree.ToArray())
+                    {
+                        if (node != null && node.GetLeft_Key() > parentRightKey)
+                        {
+                            node.setRight_Key(node.GetRight_Key() + 2);
+                            node.setLeft_Key(node.GetLeft_Key() + 2);
+                        }
+                        
+                        if (node != null && node.GetRight_Key() > parentRightKey && node.GetLeft_Key() < parentLefthKey)
+                        {
+                            node.setRight_Key(node.GetRight_Key() + 2);
+                        }
+                    }
+                   
+                    _tree.Add (new Node2(parentId, _tree.Count + 1, lefthKey, rightKey, level, newNodeName));
+                    
+                   
+                   _tree.Sort();
+                        
+                   }
+            
+            
+            }
 
-    void someMethod()
-    {
-        Node2 node = new Node2();
-        node.getNumber();
-        node.setNumberzzz(1);
-    
-        Node2.Tree2 tree = new Node2.Tree2();
-        // tree.setNum(1);
-        // tree.setNum(1);
-        
-    }
-    
-    
-    
-        
+
 }
 
--- Program.cs
+++ Program.cs
@@ -7,60 +7,77 @@ class Program
     static void Main()
     {   
         
-       Tree tree = new Tree();
+       // Tree tree = new Tree();
         
     
-       tree.AddNode(1, "1-1");
-       tree.AddNode(1, "1-2");
-       tree.AddNode(1, "1-3");
-       tree.AddNode(3, "1-2-1");
-       tree.AddNode(3, "1-2-2");
-       tree.AddNode(3, "1-2-3");
-       tree.AddNode(4, "1-3-1");
-       tree.AddNode(7, "1-2-3-1");
+       // tree.AddNode(1, "1-1");
+       // tree.AddNode(1, "1-2");
+       // tree.AddNode(1, "1-3");
+       // tree.AddNode(3, "1-2-1");
+       // tree.AddNode(3, "1-2-2");
+       // tree.AddNode(3, "1-2-3");
+       // tree.AddNode(4, "1-3-1");
+       // tree.AddNode(7, "1-2-3-1");
 
 
 
-       //find index by ID
-       int cntr = 0;
-       foreach (Node element in tree.GetTree())
-       {
-           if (element.GetId() == 4)
-           {
-               break;
-           }
-           cntr++;
-       }
-       Console.WriteLine($"cntr --- {cntr}");
+       ///////find index by ID
+       // int cntr = 0;
+       // foreach (Node element in tree.GetTree())
+       // {
+       //     if (element.GetId() == 4)
+       //     {
+       //         break;
+       //     }
+       //     cntr++;
+       // }
+       // Console.WriteLine($"cntr --- {cntr}");
 
 
-       //retrieve value by index
-       var nodara = (Node)tree.GetTree().ToArray()[7]!;
-       nodara.GetNodeValues().SetValues(0,142);
-       Console.WriteLine($"value of nodara : --- {nodara.GetNodeValues().GetValues()[0]} ---{nodara.GetParentId()}");
-
-       
+       // //retrieve value by index
+       // var nodara = (Node)tree.GetTree().ToArray()[7]!;
+       // nodara.GetNodeValues().SetValues(0,142);
+       // Console.WriteLine($"value of nodara : --- {nodara.GetNodeValues().GetValues()[0]} ---{nodara.GetParentId()}");
 
        
-       foreach (Node element in tree.GetTree())
+
+       
+       // foreach (Node element in tree.GetTree())
+       // {
+       //     if (element.GetId() == 6 )
+       //     {
+       //         element.GetNodeValues().SetValues(0, 68);
+       //     }
+       //     Console.WriteLine($"{element.GetParentId()}, {element.GetId()}, {element.GetLeft_Key()}, {element.GetRight_Key()}, {element.GetLevel()}, {element.GetName()}, == >>  {element.GetNodeValues().GetValues()[0]}");
+       // }
+
+       //=============================== test
+       
+       
+       Node2.Tree2 tree2 = new Node2.Tree2();
+       tree2.AddNode(1, "1-1");
+       tree2.AddNode(1, "1-2");
+       tree2.AddNode(1, "1-3");
+       tree2.AddNode(3, "1-2-1");
+       tree2.AddNode(3, "1-2-2");
+       tree2.AddNode(3, "1-2-3");
+       tree2.AddNode(4, "1-3-1");
+       tree2.AddNode(7, "1-2-3-1");
+       
+       
+       foreach (Node2 element in tree2.GetTree())
        {
            if (element.GetId() == 6 )
            {
                element.GetNodeValues().SetValues(0, 68);
+               int nodeval = element.GetNodeValues().GetValues()[0];
+               Console.WriteLine($" nod val {nodeval}");
            }
            Console.WriteLine($"{element.GetParentId()}, {element.GetId()}, {element.GetLeft_Key()}, {element.GetRight_Key()}, {element.GetLevel()}, {element.GetName()}, == >>  {element.GetNodeValues().GetValues()[0]}");
        }
 
-       Node2 nnn = new Node2();
-       nnn.getNumber();
-       
-       Node2.Tree2 mmmm = new Node2.Tree2();
-       
-
-
-
-
-
+       var nodara = (Node2)tree2.GetTree()[7]!;
+            
 
     }
 }

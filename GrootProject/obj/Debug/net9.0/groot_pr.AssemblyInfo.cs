31eb5f9e6d4b5f80d9c8fa5ca34861fd16ca1281
gio <tabukmail@gmail.com>
2025-01-31 15:23:28 +0000

    nuget libs removal

--- GrootProject/obj/Debug/net9.0/groot_pr.AssemblyInfo.cs
+++ GrootProject/obj/Debug/net9.0/groot_pr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("groot_pr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e813e2bbf9c225ec581283cb274bafb685b452ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bce7fdb7bc594fb88d479c21bbd2fb00106544")]
 [assembly: System.Reflection.AssemblyProductAttribute("groot_pr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("groot_pr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

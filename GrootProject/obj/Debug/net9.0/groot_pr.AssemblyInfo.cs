a504be6c2ce72a21137373ec01953cb1b8c5b8c4
bali <tabuk@mail.ee>
2025-01-31 17:23:50 +0100

    local update

--- GrootProject/obj/Debug/net9.0/groot_pr.AssemblyInfo.cs
+++ GrootProject/obj/Debug/net9.0/groot_pr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("groot_pr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8fc4804aaff26451d557c89a2dbffb697c7e3c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e813e2bbf9c225ec581283cb274bafb685b452ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("groot_pr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("groot_pr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
